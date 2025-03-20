1fb1426be5d437ab4b1036eb1f9effb31606fed2
Umarohh <umarmahmad34@gmail.com>
2025-03-20 11:49:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-def main():
-    print("Welcome to Feraz-ICS4U-GAME!")
+import pygame
+import sys
+
+#Constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+FPS = 60
+
+#Initialize Pygame
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Feraz-ICS4U-GAME")
+clock = pygame.time.Clock(FPS)
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
 
 if __name__ == "__main__":
     main()
-
-import pygame
-import sys
-pygame.init()
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-
-
