cf58e4c73c8a8a73463bc60dda060b453479a81a
fffffffffffffffffffffffffferaz <delawerf@students.dsbn.org>
2025-03-20 10:55:50 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    print("Welcome to Feraz-ICS4U-GAME!")
+
+if __name__ == "__main__":
+    main()
+
+import pygame
+import sys
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
