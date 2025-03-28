36e838626112d01adbf2c1223b62be2abb34c0db
Umarohh <umarmahmad34@gmail.com>
2025-03-28 11:03:43 -0400

    removes redundant functions in each level class by incorporating a parent level class

--- Scripts/levels.py
+++ Scripts/levels.py
@@ -28,3 +28,21 @@ class LevelManager:
     def draw(self): # level__manager.draw calls each level to render independantly
         if self.current_level:
             self.current_level.draw() # level_1.draw, level_2.draw, etc...
+
+class Level:
+    def __init__(self, screen):
+        self.screen = screen
+        self.completed = False  # Track if the level is completed
+
+    def check_completion_condition(self):
+        self.completed = True  # Level is completed if condition is met
+       
+    def is_completed(self):
+        """Return whether the level is completed to outside classes such as level_manager"""
+        return self.completed
+    
+    def update(self):   # not necessary, only for if level# classes do not have their own update method, avoiding an attribute error
+        pass
+
+    def draw(self):  # not necessary, only for if level# classes do not have their own draw method, avoiding an attribute error
+        pass
